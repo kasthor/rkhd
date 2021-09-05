f8db48735c620ecf54f830c10a3fa6f350d2da79
Giancarlo Palavicini <kasthor@gmail.com>
2021-09-05 12:52:49 -0600

    feat: some hardcoded keyboard shortcuts

---
+++ README.md
@@ -0,0 +1,43 @@
+# rkhd
+
+## Goals
+
+- Optimize the keyboard experience
+- Faster switching back and forth from aplications and to the right application
+
+## Current state
+
+Hardcoded keyboard shortcuts that use yabai for some operations
+it also throws some debugging information to the console
+
+## Shortcuts
+```
+shift + hyper + l : swap window to the righ
+shift + hyper + h : swap window to the left
+
+hyper + 0 : balance window sizes
+hyper + f : toggle full screen
+
+hyper + 1 : focus monitor 1
+hyper + 2 : focus monitor 2
+hyper + 3 : focus monitor 3
+```
+
+## Prerequisites
+
+- yabai
+- I have Capslock mapped to F18 and that's my hyper key
+
+## Motivation
+
+I spend too much time in a computer for the reason that I do development mostly all day
+I often try to bring better tools to my life so i can be more efficient during that time
+Most of my frustration comes when I have to reach the mouse, the more I can do with the keyboard the better.
+For this reason I use tools like Neovim and tmux for my development environment, it was good, the problem comes when I have to interact with the rest of the environment
+On a Mac I know a few shortcuts to be moving around thru some of the windows, and switching spaces, which mostly helps, but I've been found frustrated from time to time trying to reach the window I need
+So in order to fix this problem I've reached tools such as [yabai](https://github.com/koekeishiya/yabai) and [skhd](https://github.com/koekeishiya/skhd), also there is a tool called 'Snap' and 'Amethyst'
+During my research I've previously found a guy on youtube that was sharing his setup and shared one interesting idea.
+As stated before searching for windows is a mess, and the thing is that moslty all window managers are navigated in a "relative" manner, it would be better to have an absolute way to find the window with just a keyboard shortcut
+For my last setup I was thinking about trying yabai with skhd, but well, I have no more modifier keys, and I'm affraid I will be having conflicts with other keyboard shortcuts, so I've seen that there is the "Hyper" key, but to set it up, skhd requires additional tools, and I just don't want to go that path, plus those tools appear not to be working in the latest versions of OSX
+And well, I wanted to know if there was a better way to do it from skhd, but then I thought it could be an opportunity to learn rust, so here I am.
+I'll attempt to create this project to help me finetune my multi-monitor desktop experience and my head is quite exploding, at first I will try to get something similar to skhd for the keyboard shortcuts, but really don't know where this is going to end, or else I might end up getting to a Linux machine instead
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ extern crate libc;
 #[link(name = "CoreFoundation", kind = "framework")]
 #[link(name = "CoreGraphics", kind = "framework")]
 
+use std::ptr;
+use std::process::Command;
+
 pub type CFRunLoopRef = *const libc::c_void;
 pub type CGEventType = libc::c_uint;
 pub type CGEventTapProxy = *const libc::c_void;
@@ -19,7 +22,6 @@ pub type CFBooleanRef = *const __CFBoolean;
 pub static kCGEventKeyDown: CGEventType = 10;
 pub static kCGEventKeyUp: CGEventType = 11;
 pub static kCGEventFlagsChanged: CGEventType = 12;
-use std::ptr;
 
 pub type CGEventTapCallBack = extern fn(CGEventTapProxy, CGEventType,
                                         CGEventRef, *const libc::c_void)
@@ -29,6 +31,16 @@ pub type CGEventTapCallBack = extern fn(CGEventTapProxy, CGEventType,
 #[non_exhaustive]
 pub enum EventField {
     KeyboardEventKeycode = 9
+}
+
+pub enum FlagType {
+    None = 0,
+    Control = 1 << 0,
+    Option  = 1 << 1,
+    Command = 1 << 2,
+    Shift = 1 << 3,
+    Fn = 1 << 4,
+    Hyper = 1 << 5,
 }
 
 extern {
@@ -52,20 +64,98 @@ extern {
     pub fn CFRunLoopRun();
 }
 
-extern fn event_tap_callback(_: CGEventTapProxy, event_type: CGEventType, event: CGEventRef, arg: *const libc::c_void) -> CGEventRef {
+static mut flags:u16 = 0;
 
+extern fn event_tap_callback(_: CGEventTapProxy, event_type: CGEventType, event: CGEventRef, arg: *const libc::c_void) -> CGEventRef {
     let keycode = unsafe{CGEventGetIntegerValueField(event, EventField::KeyboardEventKeycode)} as u16;
 
+    let flag = flag_from_key(keycode);
+    let mut should_capture_event = false;
 
-    println!("event_type: {:?}, key: {:?}", event_type, keycode);
-    event
+    if ! matches!(flag, FlagType::None) {
+        set_flags(flag, event_type);
+        should_capture_event = event_type == kCGEventKeyDown; // If the key is not a flag key but it's detected as a flag then the event should be captured
+    } else {
+        if event_type == kCGEventKeyDown {
+            should_capture_event = find_and_exec_key(keycode);
+        }
+    }
+
+    unsafe{ println!("event_type: {:?}, key: {:#X}, flag: {:07b}", event_type, keycode, flags as u16) };
+
+    if should_capture_event { ptr::null() } else { event }
+}
+
+fn find_and_exec_key(key: u16) -> bool{
+    unsafe {
+        if flags == FlagType::Hyper as u16 | FlagType::Shift as u16 && key == 0x25 {
+            run_command("yabai -m window --swap east");
+            true
+        } else if flags == FlagType::Hyper as u16 | FlagType::Shift as u16 && key == 0x04 {
+            run_command("yabai -m window --swap west");
+            true
+        } else if flags == FlagType::Hyper as u16 && key == 0x18 {
+            run_command("yabai -m space --balance");
+            true
+        } else if flags == FlagType::Hyper as u16 && key == 0x03 {
+            run_command("yabai -m window --toggle native-fullscreen");
+            true
+        } else if flags == FlagType::Hyper as u16 && key == 0x12 {
+            run_command("yabai -m display --focus 1");
+            true
+        } else if flags == FlagType::Hyper as u16 && key == 0x13 {
+            run_command("yabai -m display --focus 2");
+            true
+        } else if flags == FlagType::Hyper as u16 && key == 0x14 {
+            run_command("yabai -m display --focus 3");
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn run_command(command: &str) {
+    Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .spawn()
+        .expect("failed to run");
+}
+
+fn set_flags(flag: FlagType, event_type: CGEventType) {
+    let mask = flag as u16;
+
+    unsafe{
+        if event_type == kCGEventFlagsChanged {
+            flags = flags ^ mask
+        } else if event_type == kCGEventKeyDown {
+            flags = flags | mask
+        } else if event_type == kCGEventKeyUp {
+            flags = flags ^ mask
+        } 
+    }
+}
+
+fn flag_from_key(keycode:u16) -> FlagType{
+    match keycode {
+        0x3B => FlagType::Control,
+        0x3A => FlagType::Option,
+        0x3D => FlagType::Option, // Right
+        0x36 => FlagType::Command, // Right
+        0x37 => FlagType::Command,
+        0x38 => FlagType::Shift,
+        0x3C => FlagType::Shift, // Right
+        0x3F => FlagType::Fn,
+        0x4F => FlagType::Hyper,
+        _ => FlagType::None
+    }
 }
 
 fn main() {
     let keydown = 1 << kCGEventKeyDown;
     let keyup = 1 << kCGEventKeyUp;
     let modifier = 1 << kCGEventFlagsChanged;
-
 
     unsafe {
         let tap = CGEventTapCreate(0, 0, 0, modifier | keydown | keyup, event_tap_callback, ptr::null());
@@ -74,7 +164,7 @@ fn main() {
             panic!("Not enough priviledges");
         }
         let source = CFMachPortCreateRunLoopSource(
-            ::std::ptr::null(), tap, 0
+            ptr::null(), tap, 0
             );
 
         let run_loop = CFRunLoopGetCurrent();
